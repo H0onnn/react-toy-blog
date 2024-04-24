057ccb1ba159417ef095442cdcf8ef9ba484dead
H0onnn <dukei2017@naver.com>
2024-04-24 10:08:21 +0900

    Feat write button component

---
+++ src/shared/components/ui/WriteButton.tsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import { Button, ButtonProps } from "./Button";
+
+interface Props extends ButtonProps {
+  children: React.ReactNode;
+  href: string;
+  imgSize?: number;
+}
+
+export const WriteButton = ({
+  children,
+  href,
+  imgSize = 24,
+  ...props
+}: Props) => {
+  return (
+    <Link to={href}>
+      <Button
+        {...props}
+        rightSlot={
+          <img
+            src="/icons/plus.svg"
+            alt="plus"
+            width={imgSize}
+            height={imgSize}
+          />
+        }
+      >
+        {children}
+      </Button>
+    </Link>
+  );
+};
