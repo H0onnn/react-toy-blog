51c20b21f1033c2c5f4e95f090b90f3e0fc30222
H0onnn <dukei2017@naver.com>
2024-04-24 10:06:18 +0900

    Feat PageLayout

---
+++ src/shared/components/layout/PageLayout.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+import { Outlet } from "react-router-dom";
+
+import { SideNavBar } from "./SideNavBar";
+
+export const PageLayout = () => {
+  return (
+    <Layout>
+      <SideNavBar />
+      <main>
+        <Outlet />
+      </main>
+    </Layout>
+  );
+};
+
+const Layout = styled.div`
+  height: 100vh;
+  display: flex;
+
+  & > main {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    height: 100%;
+    padding: 2.4rem;
+    margin-left: 26rem;
+    overflow-y: auto;
+  }
+`;
