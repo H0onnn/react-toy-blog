0f0fe4d5c9c3708ed9d4984152753160011bb4f8
H0onnn <dukei2017@naver.com>
2024-04-24 10:06:46 +0900

    Feat nav menu item constants

---
+++ src/shared/constants/Nav.ts
@@ -0,0 +1,14 @@
+export const MENU_ITEMS = [
+  {
+    title: "All Posts",
+    icon: "/icons/post.svg",
+    icon_fill: "/icons/post-fill.svg",
+    href: "/",
+  },
+  {
+    title: "Profile",
+    icon: "/icons/profile.svg",
+    icon_fill: "/icons/profile-fill.svg",
+    href: "/profile",
+  },
+] as const;
