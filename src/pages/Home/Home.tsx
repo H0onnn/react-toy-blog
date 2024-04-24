0f89f8ddda3a2566610be211d936744bffcbd02c
H0onnn <dukei2017@naver.com>
2024-04-24 10:11:10 +0900

    Feat main page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+import { usePosts } from "@/features/post/apis";
+import { PostList } from "@/features/post/components";
+import { Flex, WriteButton } from "@/shared/components/ui";
+
+const Home = () => {
+  const { posts } = usePosts();
+
+  return (
+    <>
+      <Flex align="center" justify="space-between" style={{ margin: "2rem 0" }}>
+        <Header>H0onnn's react blog</Header>
+
+        <WriteButton href="/post/new">게시글 작성</WriteButton>
+      </Flex>
+
+      <PostList posts={posts} />
+    </>
+  );
+};
+
+export default Home;
+
+const Header = styled.h1`
+  font-size: 2.5rem;
+  font-weight: bold;
+`;
