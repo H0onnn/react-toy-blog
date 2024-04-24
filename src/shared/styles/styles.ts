e701e5971c24b927cdebb7678cecf6abe13852d8
H0onnn <dukei2017@naver.com>
2024-04-24 10:03:23 +0900

    Design thems constants, common css

---
+++ src/shared/styles/styles.ts
@@ -0,0 +1,13 @@
+import { css } from "styled-components";
+
+export const text_hidden = css`
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+`;
+
+export const full_img_cover = css`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
---
+++ src/shared/styles/themes.ts
@@ -0,0 +1,12 @@
+export const THEMES = {
+  primary: "#2F6FEB",
+  hover: "#2D68E3",
+  secondary: "#E0F2FE",
+  secondaryHover: "#D3EAFD",
+  gray: "#F9FAFB",
+  mediumgray: "#D1D5DB",
+  darkgray: "#6B7280",
+  buttongray: "#E5E7EB",
+  black: "#1F2937",
+  white: "#FFFFFF",
+} as const;
