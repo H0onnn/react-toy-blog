809d549af811ee39f3d0e69271be0e7c0501bbbe
H0onnn <dukei2017@naver.com>
2024-04-24 10:09:17 +0900

    Feat comment form

---
+++ src/features/comment/components/comment-write/CommentForm.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useWriteComment } from "../../apis";
+import { Flex, Input, Button } from "@/shared/components/ui";
+
+export const CommentForm = () => {
+  const { writeComment } = useWriteComment();
+
+  const { id } = useParams<{ id: string }>();
+
+  const [comment, setComment] = useState("");
+
+  const handleSubmit = async () => {
+    const newComment = await writeComment({
+      id: crypto.randomUUID(),
+      postId: id as string,
+      content: comment,
+      createdAt: new Date().toISOString(),
+    });
+
+    if (newComment) {
+      setComment("");
+      window.location.reload();
+    } else {
+      window.alert("댓글 작성중 오류가 발생했습니다.");
+    }
+  };
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit();
+      }}
+      style={{ width: "100%", marginTop: "2rem" }}
+    >
+      <Flex gap={20}>
+        <Input
+          placeholder="댓글을 입력해주세요."
+          required
+          value={comment}
+          onVChange={(value) => {
+            setComment(value);
+          }}
+          leftSlot={
+            <img
+              src="/icons/pencil.svg"
+              alt="pencil"
+              style={{ width: 24, height: 24 }}
+            />
+          }
+        />
+
+        <Button
+          type="submit"
+          rightSlot={
+            <img src="/icons/plus.svg" alt="plus" width={24} height={24} />
+          }
+        >
+          댓글 작성
+        </Button>
+      </Flex>
+    </form>
+  );
+};
